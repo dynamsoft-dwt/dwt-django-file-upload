546a29a7ecad83c5c9cce596d54f37fbfa590478
Desmond <desmond@dynamsoft.com>
2015-06-17 11:13:43 +0800

    form upload

---
+++ form-upload/simpleform/formupload/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def home(request):
+    return render(request, 'index.htm', {'what':'Django File Upload'})
+
+def upload(request):
+    if request.method == 'POST':
+        handle_uploaded_file(request.FILES['file'], str(request.FILES['file']))
+        return HttpResponse("Successful")
+
+    return HttpResponse("Failed")
+
+
+def handle_uploaded_file(file, filename):
+    with open('upload/' + filename, 'wb+') as destination:
+        for chunk in file.chunks():
+            destination.write(chunk)
