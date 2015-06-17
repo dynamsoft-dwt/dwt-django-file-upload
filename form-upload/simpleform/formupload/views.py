77a2eebfcde872d8ca11e1e43c7cdb13dfa1704a
Desmond <desmond@dynamsoft.com>
2015-06-17 14:35:49 +0800

    for-upload: check upload dir

--- form-upload/simpleform/formupload/views.py
+++ form-upload/simpleform/formupload/views.py
@@ -12,8 +12,10 @@ def upload(request):
 
     return HttpResponse("Failed")
 
-
 def handle_uploaded_file(file, filename):
+    if not os.path.exists('upload/'):
+        os.mkdir('upload/')
+
     with open('upload/' + filename, 'wb+') as destination:
         for chunk in file.chunks():
             destination.write(chunk)
